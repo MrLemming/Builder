c2cb086b14f77093a3cb148545de07ea4bc53e69
Aleksey Esin <aesin87@mail.ru>
2022-11-17 20:01:36 +0300

    Initial commit

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,77 @@
+package org.example;
+
+import java.util.Objects;
+import java.util.OptionalInt;
+
+public class Person {
+    protected final String name;
+    protected final String surname;
+    protected OptionalInt age;
+    protected String city;
+
+    public Person(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public Person(String name, String surname, OptionalInt age) {
+        this(name, surname);
+        this.age = age;
+    }
+
+    public Person(String name, String surname, OptionalInt age, String city) {
+        this(name, surname, age);
+        this.city = city;
+    }
+
+    public boolean hasAge() {
+        return age.isPresent();
+    }
+
+    public boolean hasAddress() {
+        return city != null;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public OptionalInt getAge() {
+        return age;
+    }
+
+    public String getAddress() {
+        return city;
+    }
+
+    public void setAddress(String city) {
+        this.city = city;
+    }
+
+    public void happyBirthday() {
+        age = OptionalInt.of(age.getAsInt() + 1);
+    }
+
+    @Override
+    public String toString() {
+        return name + ' ' + surname + " возрастом " + age + " лет, что проживает в городе " + city;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname, age, city);
+    }
+
+    public PersonBuilder newChildBuilder() {
+        return new PersonBuilder()
+                .setSurname(surname)
+                .setAge(0)
+                .setAddress(city);
+    }
+}
+
+
---
+++ src/main/java/org/example/PersonBuilder.java
@@ -0,0 +1,50 @@
+package org.example;
+
+import java.util.OptionalInt;
+
+public class PersonBuilder {
+    private String name;
+    private OptionalInt age;
+    private String city;
+    private String surname;
+    Person person;
+
+    public PersonBuilder setName(String name) {
+        this.name = name;
+        return PersonBuilder.this;
+    }
+
+    public PersonBuilder setSurname(String surname) {
+        this.surname = surname;
+        return PersonBuilder.this;
+    }
+
+    public PersonBuilder setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException("Указан неверный возраст. ");
+        } else {
+            this.age = OptionalInt.of(age);
+            return this;
+        }
+    }
+
+    public PersonBuilder setAddress(String city) {
+        this.city = city;
+        return PersonBuilder.this;
+    }
+
+    public Person build() {
+        if (name == null || surname == null) {
+            throw new IllegalStateException("Не указаны необходимые данные. Поля Имя и Фамилия являются обязательными.");
+        }
+        if (age.isPresent()) {
+            person = new Person(name, surname, age);
+        } else {
+            person = new Person(name, surname);
+        }
+        if (city != null) {
+            person = new Person(name, surname, age, city);
+        }
+        return person;
+    }
+}
